61e25a20f39f8bcf79b36a986bab78fdf27303dd
Thai Vo <tahrbo@gmail.com>
2025-07-22 22:40:42 -0500

    taking a fucking break

--- sqlJoins.sql
+++ sqlJoins.sql
@@ -2,11 +2,19 @@
 
 using the products table and the categories table, return the product name and the category name */
 
+select products.name as 'Product', categories.name as 'Category Name' from products inner join categories on products.categoryid = categories.categoryid where products.categoryid = 1;
+
 /* joins: find all product names, product prices, and products ratings that have a rating of 5 */
+
+select products.name as "Name", products.price as "Price", reviews.rating as "Rating" from  products inner join reviews on products.productid = reviews.ProductID where reviews.rating = 5;
 
 /* joins: find the employee with the most total quantity sold.  use the sum() function and group by */
 
+select employees.FirstName, employees.LastName from employees inner join sales group by sum(sales.Quantity) order by sales.Quantity;
+
 /* joins: find the name of the department, and the name of the category for Appliances and Games */
+
+select categories.name as 'Category Name', departments.name as 'Department Name' from categories inner join departments on categories.departmentid = departments.departmentid where categories.name = 'Appliances' or categories.name = 'Games';
 
 /* joins: find the product name, total # sold, and total price sold,
 
